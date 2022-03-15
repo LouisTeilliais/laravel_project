5f6de1f7cf991f8150d91a3724fd4816afe9d187
t.bourmaud <tao.bourmaud@ynov.com>
2022-03-15 16:14:31 +0100

    update table with timestamps

--- database/migrations/2022_03_15_131751_create_table_softs.php
+++ database/migrations/2022_03_15_131751_create_table_softs.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('softs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->timestamps();
         });
     }
 
--- database/migrations/2022_03_15_131846_create_table_types_alcohol.php
+++ database/migrations/2022_03_15_131846_create_table_types_alcohol.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('alcohol', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->timestamps();
         });
     }
 
