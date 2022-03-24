9c9aabdace402c02b005b87bbc124f81506bf5e8
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-03-24 15:42:16 +0100

    Fonctionnalité - Update boisson créée

--- resources/views/alcohol/softs.blade.php
+++ resources/views/alcohol/softs.blade.php
@@ -9,32 +9,35 @@
 <body>
     <a href=" {{ route('home')}}">Retour au menu</a>
     <h1>Softs</h1>
-
-    <table border=1> 
-        <thead>
-            <tr>
-                <th> id </th>
-                <th> name </th>
-                <th> button </th>
-            </tr>
-        </thead>
-        <tbody>
-            @if(!is_null($softs) && !empty($softs))
-                @foreach($softs as $soft)
+    @if(!is_null($softs) && !empty($softs))
+        @foreach($softs as $soft)
+            <table border=1> 
+                <thead>
                     <tr>
-                        <td> {{  $soft->id}}   </td>
-                        <td> {{  $soft->name}}   </td>
-                        <td>
-                            {{-- <a id="mod" href="{{route('softs.modifier', $soft->id )}}">Modifier</a> --}}
-                            <a id="del" href="{{route('softs.delete', $soft->id )}}">Supprimer</a>
-                        </td>
+                        <th> id </th>
+                        <th> name </th>
+                        <th> button </th>
                     </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
-
-
+                </thead>
+                <tbody>
+                            <tr>
+                                <td> {{  $soft->id}}   </td>
+                                <td> {{  $soft->name}}   </td>
+                                <td>
+                                    {{-- <a id="mod" href="{{route('softs.modifier', $soft->id )}}">Modifier</a> --}}
+                                    <a id="del" href="{{route('softs.delete', $soft->id )}}">Supprimer</a>
+                                </td>
+                            </tr>
+                </tbody>
+            </table>
+            <form action=" {{ route('softs.update', $soft->id) }}" method="POST">
+                @csrf
+                @method("PUT")
+                <input type="text" name="name2" value="{{$soft->name}}">
+                <button type="submit" > Modifier boisson soft </button>
+            </form>
+        @endforeach
+    @endif
     <form action=" {{ route('softs.create') }}" method="POST">
         @csrf
         <input type="text" name="name2">
