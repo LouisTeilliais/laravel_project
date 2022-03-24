9d0bbf7ecc9038441473ffd72b6276868b1bd4c7
Louis Teilliais <louis.teilliais@ynov.com>
2022-03-24 16:56:08 +0100

    style card

--- resources/views/alcohol/sirop.blade.php
+++ resources/views/alcohol/sirop.blade.php
@@ -4,45 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <title>Ajouter sirop</title>
 </head>
 <body>
     <a href=" {{ route('home')}}">Retour au menu</a>
-    <h1>Liste de sirop</h1>
-    @if(!is_null($sirop) && !empty($sirop))
-        @foreach($sirop as $sirops)
-            <table border=1> 
-                <thead>
-                    <tr>
-                        <th> id </th>
-                        <th> name </th>
-                        <th> button </th>
-                    </tr>
-                </thead>
-                <tbody>
-                            <tr>
-                                <td> {{  $sirops->id}}   </td>
-                                <td> {{  $sirops->name}}   </td>
-                                <td>
-                                    <!-- {{-- <a id="mod" href="{{route('sirop.modifier', $sirops->id )}}">Modifier</a> --}} -->
-                                    <a id="del" href="{{route('sirop.delete', $sirops->id )}}">Supprimer</a>
-                                </td>
-                            </tr>
-                </tbody>
-            </table>
-            <form action=" {{ route('sirop.update', $sirops->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <input type="text" name="add" value="{{$sirops->name}}">
-                <button type="submit" > Modifier boisson sirops </button>
-            </form>
-        @endforeach
-    @endif
+    <h1>Liste de Sirop</h1>
+
     <form action=" {{ route('sirop.create') }}" method="POST">
         @csrf
-        <input type="text" name="add">
-        <button type="submit" > Ajouter boisson sirops </button>
+        <input  type="text" name="add">
+        <button type="submit"  class="btn btn-success"> Ajouter boisson sirops </button>
     </form>
+
+    @if(!is_null($sirop) && !empty($sirop))
+        @foreach($sirop as $sirops)    
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+            <div class="card text-center" style="width: 18rem;">
+                <div class="card-body">
+                <h5> Nom du sirop :  {{  $sirops->name}}   </h5>
+                <p> Id : {{$sirops->id}}</p>
+                <p> Ajouté le : {{$sirops->created_at}}</p>
+                <p> Modifié le : {{$sirops->updated_at}}</p>
+                <p> </p>
+                </div>
+                                
+                <a class="btn btn-danger" id="del" href="{{route('sirop.delete', $sirops->id )}}">Supprimer</a>
+                <form action=" {{ route('sirop.update', $sirops->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <button type="submit" class="btn btn-warning"> Modifier boisson sirops </button>
+                <input class="form-control" type="text" name="add" value="{{$sirops->name}}">
+                
+                </form>
+                </div>       
+            </div>
+        </div>
+        @endforeach
+    @endif
+    
 
 
 </body>
