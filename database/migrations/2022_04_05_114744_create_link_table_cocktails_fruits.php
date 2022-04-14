206ba3861323d96981ea934d60ba150acd4e3b79
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-04-14 10:10:47 +0200

    Modifier image -> verres + fruits / Modifier dégré alcool / Navbar ajout du lien vers softs + sirop + cocktails / style CSS des boutons avec valeur actuel dedans

--- database/migrations/2022_04_05_114744_create_link_table_cocktails_fruits.php
+++ database/migrations/2022_04_05_114744_create_link_table_cocktails_fruits.php
@@ -14,10 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cocktails_fruits', function (Blueprint $table) {
-            $table->unsignedBigInteger('cocktails_id')->nullable();
+            $table->unsignedBigInteger('cocktail_id')->nullable();
             $table->unsignedBigInteger('fruits_id')->nullable();
             $table->foreign('fruits_id')->references('id')->on('fruits');
-            $table->foreign('cocktails_id')->references('id')->on('cocktails');
+            $table->foreign('cocktail_id')->references('id')->on('cocktails');
             $table->timestamps();
         });
     }
